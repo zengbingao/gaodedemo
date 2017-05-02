8e55ad305750fd461a74091791f512deda78db22
RobinGao <robin.gao@imaginato.com>
2017-05-02 17:58:11 +0800

    Initial commit高德地图demo

---
+++ app/src/main/java/com/robin/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.robin.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps.MapView;
+
+import static com.robin.myapplication.R.id.map;
+
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(map);
+        mMapView.onCreate(savedInstanceState);// 此方法必须重写
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+}
